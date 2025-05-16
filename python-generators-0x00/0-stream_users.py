c1bac79554ab15c96ee632ba401c8e5f50ef743a
Achiugo Trust <achiugotrust@gmail.com>
2025-05-16 11:04:26 +0100

    stream_users

--- python-generators-0x00/0-stream_users.py
+++ python-generators-0x00/0-stream_users.py
@@ -1,12 +1,21 @@
 import mysql.connector
-from seed import connect_to_prodev
 
-def stream_users(connection):
-    try: 
+
+def stream_users():
+    try:
+        connection = mysql.connector.connect(
+            host="localhost",
+            user="root",        # Replace with your MySQL username
+            password="96946765", # Replace with your MySQL password
+            database="ALX_prodev"
+        )
+        
         cursor = connection.cursor(dictionary=True)  # Fetch results as dictionaries
 
+        # Step 2: Execute the SQL query to fetch all users
         cursor.execute("SELECT * FROM user_data")
 
+        # Step 3: Use a generator to yield each user row
         for user in cursor:
             yield user
 
